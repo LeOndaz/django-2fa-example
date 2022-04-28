e4f4fee76daef37b5e4e193391dbcac3b7256642
Ahmed Ibrahim <leondaz@Ahmeds-MacBook-Pro.local>
2022-04-29 01:51:30 +0200

    fix typo

--- auth_example/settings.py
+++ auth_example/settings.py
@@ -98,7 +98,7 @@ if DEBUG:
         "NAME": BASE_DIR / "db.sqlite3",
     }
 else:
-    DATABASES["default"] = (dj_database_url.config(conn_max_age=600),)
+    DATABASES["default"] = dj_database_url.config(conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
